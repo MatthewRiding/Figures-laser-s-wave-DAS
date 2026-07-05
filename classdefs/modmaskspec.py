ad744bd126dd2e60b45e29ac18cc6912fc8906b6
MatthewRiding <116739540+MatthewRiding@users.noreply.github.com>
2026-07-05 10:53:03 +0100

    Removed folder

--- classdefs/modmaskspec.py
+++
@@ -1,4 +0,0 @@
-class MaskSpec:
-    def __init__(self, mask_angle_deg, mask_behaviour):
-        self.mask_angle_deg = mask_angle_deg
-        self.mask_behaviour = mask_behaviour
--- classdefs/modwaveset.py
+++
@@ -1,4 +0,0 @@
-class WaveSet:
-    def __init__(self, travel_time_function, delay_law_function):
-        self.travel_time_function = travel_time_function
-        self.delay_law_function = delay_law_function
